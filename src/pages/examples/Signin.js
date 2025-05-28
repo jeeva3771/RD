556b9fa5ec5a6a40901f2f1d7046654584bb0d7b
mr sivasankar <jeevanantham.s@creativebees.in>
2025-05-28 13:44:34 +0530

    add folder

---
+++ src/pages/examples/Signin.js
@@ -0,0 +1,98 @@
+
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookF, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { Col, Row, Form, Card, Button, FormCheck, Container, InputGroup } from '@themesberg/react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import { Routes } from "../../routes";
+// import BgImage from "../../assets/img/illustrations/signin.svg";
+import logo from "../../logo.svg"
+
+
+export default () => {
+   const [showPassword, setShowPassword] = useState(false)
+
+  const togglePassword = () => setShowPassword(!showPassword);
+  return (
+    <main>
+      <section className="vh-100 d-flex justify-content-center align-items-center">
+        <Container>
+        
+          <Row className="justify-content-center form-bg-image">
+                      {/* <Row className="justify-content-center form-bg-image" style={{ backgroundImage: `url(${BgImage})` }}> */}
+
+            <Col xs={12} className="d-flex align-items-center justify-content-center">
+              <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+                <div className="text-center text-md-center mb-4 mt-md-0">
+                  <img src={logo} />
+                  <h3 className="mb-0 mt-3">Sign in to our platform</h3>
+                </div>
+                <Form className="mt-4">
+                  <Form.Group id="email" className="mb-4">
+                    <Form.Label>Your Email</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                      </InputGroup.Text>
+                      <Form.Control autoFocus required type="email" placeholder="example@company.com" />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Group id="password" className="mb-4">
+                      <Form.Label>Your Password</Form.Label>
+                      <InputGroup>
+                        <InputGroup.Text>
+                          <FontAwesomeIcon icon={faUnlockAlt} />
+                        </InputGroup.Text>
+                        <Form.Control required
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Password" />
+                      </InputGroup>
+                    </Form.Group>
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                      <Form.Check type="checkbox">
+                        <FormCheck.Input id="defaultCheck5" className="me-2" onChange={togglePassword}/>
+                        <FormCheck.Label htmlFor="defaultCheck5" className="mb-0" >Show password</FormCheck.Label>
+                      </Form.Check>
+                      <Card.Link className="small text-end">Lost password?</Card.Link>
+                    </div>
+                  </Form.Group>
+                  <Button variant="primary" type="submit" className="w-100" onClick={Routes.Signup.path}>
+                    Sign in
+                  </Button>
+                </Form>
+
+                {/* <div className="mt-3 mb-4 text-center">
+                  <span className="fw-normal">or login with</span>
+                </div>
+                <div className="d-flex justify-content-center my-4">
+                  <Button variant="outline-light" className="btn-icon-only btn-pill text-facebook me-2">
+                    <FontAwesomeIcon icon={faFacebookF} />
+                  </Button>
+                  <Button variant="outline-light" className="btn-icon-only btn-pill text-twitter me-2">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </Button>
+                  <Button variant="outline-light" className="btn-icon-only btn-pil text-dark">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </Button>
+                </div> */}
+                <div className="d-flex justify-content-center align-items-center mt-4">
+                  <span className="fw-normal">
+                    Not registered?
+                    {/* <Card.Link as={Link} to={Routes.Signup.path} className="fw-bold"> */}
+                    
+                    <Card.Link  className="fw-bold">
+                      {` Create account `}
+                    </Card.Link>
+                  </span>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </main>
+  );
+};
